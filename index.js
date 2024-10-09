4edfeec35d18af149fb26f9dd459732be81cbddb
Antony Kimanzi <antonykimanzi29@gmail.com>
2024-10-09 19:20:31 +0300

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    input.addEventListener("click", ()=> {
+        alert("Button clicked!");
+    });
 }
+
+addingEventListener();
